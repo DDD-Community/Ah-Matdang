4a3316a479f30694cb5c77c8ba89f03164e805e4
yxxngjxn <yxxngjxn99@gmail.com>
2025-08-16 14:05:29 +0900

    refactor : 최초 로그인 여부를 확인하기 위한 provider id 검색 로직 추가

--- src/main/java/be/ddd/domain/repo/MemberRepositoryCustom.java
+++ src/main/java/be/ddd/domain/repo/MemberRepositoryCustom.java
@@ -3,7 +3,9 @@ package be.ddd.domain.repo;
 import be.ddd.domain.entity.member.Member;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepositoryCustom {
     List<Member> findAllByNotificationEnabledAndReminderTime(LocalTime reminderTime);
+    Optional<Member> findByProviderId(String providerId);
 }
